11165fe4306a79b07e2cdf09fce51eb70ddd8b77
pingvin <elektron_pochta@mail.ru>
2011-11-29 12:29:41 +0600

    Add new folder /firmware/Source

---
+++ firmware/Source/default/dep/.svn/text-base/global_variables.o.d.svn-base
@@ -0,0 +1,48 @@
+global_variables.o: ../global_variables.c \
+  d:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
+  d:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  d:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
+  d:/winavr-20100110/lib/gcc/../../avr/include/stdint.h \
+  d:/winavr-20100110/lib/gcc/../../avr/include/avr/iom16.h \
+  d:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h \
+  d:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h \
+  d:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h \
+  d:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
+  d:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h \
+  d:/winavr-20100110/lib/gcc/../../avr/include/util/delay.h \
+  d:/winavr-20100110/lib/gcc/../../avr/include/util/delay_basic.h \
+  d:/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h \
+  d:/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h \
+  d:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h ../types.h
+
+d:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h:
+
+d:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+d:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h:
+
+d:/winavr-20100110/lib/gcc/../../avr/include/stdint.h:
+
+d:/winavr-20100110/lib/gcc/../../avr/include/avr/iom16.h:
+
+d:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h:
+
+d:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h:
+
+d:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h:
+
+d:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h:
+
+d:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h:
+
+d:/winavr-20100110/lib/gcc/../../avr/include/util/delay.h:
+
+d:/winavr-20100110/lib/gcc/../../avr/include/util/delay_basic.h:
+
+d:/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h:
+
+d:/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h:
+
+d:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h:
+
+../types.h:
---
+++ firmware/Source/default/dep/ltag_ascetic.o.d
@@ -0,0 +1,58 @@
+ltag_ascetic.o: ../ltag_ascetic.c ../ltag_ascetic.h \
+  d:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h \
+  d:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h \
+  d:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h \
+  d:/winavr-20100110/lib/gcc/../../avr/include/stdint.h \
+  d:/winavr-20100110/lib/gcc/../../avr/include/avr/iom16.h \
+  d:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h \
+  d:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h \
+  d:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h \
+  d:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h \
+  d:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h \
+  d:/winavr-20100110/lib/gcc/../../avr/include/util/delay.h \
+  d:/winavr-20100110/lib/gcc/../../avr/include/util/delay_basic.h \
+  d:/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h \
+  d:/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h \
+  d:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h \
+  ../definition_of_ports_atmega16.h ../hal.h ../miles_protocol.h \
+  ../types.h
+
+../ltag_ascetic.h:
+
+d:/winavr-20100110/lib/gcc/../../avr/include/avr/io.h:
+
+d:/winavr-20100110/lib/gcc/../../avr/include/avr/sfr_defs.h:
+
+d:/winavr-20100110/lib/gcc/../../avr/include/inttypes.h:
+
+d:/winavr-20100110/lib/gcc/../../avr/include/stdint.h:
+
+d:/winavr-20100110/lib/gcc/../../avr/include/avr/iom16.h:
+
+d:/winavr-20100110/lib/gcc/../../avr/include/avr/portpins.h:
+
+d:/winavr-20100110/lib/gcc/../../avr/include/avr/common.h:
+
+d:/winavr-20100110/lib/gcc/../../avr/include/avr/version.h:
+
+d:/winavr-20100110/lib/gcc/../../avr/include/avr/fuse.h:
+
+d:/winavr-20100110/lib/gcc/../../avr/include/avr/lock.h:
+
+d:/winavr-20100110/lib/gcc/../../avr/include/util/delay.h:
+
+d:/winavr-20100110/lib/gcc/../../avr/include/util/delay_basic.h:
+
+d:/winavr-20100110/lib/gcc/../../avr/include/avr/interrupt.h:
+
+d:/winavr-20100110/lib/gcc/../../avr/include/avr/pgmspace.h:
+
+d:\winavr-20100110\bin\../lib/gcc/avr/4.3.3/include/stddef.h:
+
+../definition_of_ports_atmega16.h:
+
+../hal.h:
+
+../miles_protocol.h:
+
+../types.h:
